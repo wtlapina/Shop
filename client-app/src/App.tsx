10f1bb8f9729b35a96a677e95eb271c7860583c5
wtlapina <walter_lapina@ligph.com>
2020-01-24 11:43:53 +0800

    added client app

---
+++ client-app/src/App.tsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Header, Icon, List } from 'semantic-ui-react';
+import axios from 'axios';
+
+
+class App extends Component {
+  state = {
+    values: []
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/values')
+      .then((response) => {
+        this.setState({
+          values: response.data
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header as="h2">
+          <Icon name="plug" />
+          <Header.Content>Uptime Guarantee</Header.Content>
+        </Header>
+        <List>
+          
+            {this.state.values.map((value: any) => {
+            <List.Item key={value.id}>{value.name}</List.Item>
+            })}
+          
+        </List>
+      </div>
+    );
+  }
+}
+
+export default App;
